b18b4b88016f3d3a542bc92a1ef0e5342a61e6b4
PaulBriand <briand.paul@outlook.fr>
2021-11-16 09:59:51 +0100

    Ajout Catégorie visuel

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Tag;
 use App\Entity\Task;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -13,13 +14,27 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
-        for ($t = 0; $t < mt_rand(15, 30); $t++) {
-            $task = new Task();
+        for ($c = 0; $c < 5; $c++) {
 
+            $tag = new Tag();
+            $tag->setName($faker->colorName());
+
+            $manager->persist($tag);
+        }
+
+        $manager->flush();
+
+        $allTags = $manager->getRepository(Tag::class)->findAll();
+
+
+        for ($t = 0; $t < mt_rand(15, 30); $t++) {
+
+            $task = new Task();
             $task->setName($faker->sentence(6));
             $task->setDescription($faker->paragraph(3));
             $task->setCreatedAt(new \DateTime());
             $task->setDueAt($faker->dateTimeBetween('now', '15 days'));
+            $task->setTag($faker->randomElement($allTags));
 
             $manager->persist($task);
         }
