0ac60fb0c59746c7de819579c4e818352774e3f7
PaulBriand <briand.paul@outlook.fr>
2021-11-17 10:35:43 +0100

    Fixtures User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,14 +2,27 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Tag;
 use App\Entity\Task;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     *
+     * @var UserPasswordHasherInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
@@ -37,6 +50,21 @@ class AppFixtures extends Fixture
             $task->setTag($faker->randomElement($allTags));
 
             $manager->persist($task);
+        }
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User;
+            $hash = $this->encoder->hashPassword($user, "password");
+            $user->setPassword($hash);
+
+            if ($u === 0) {
+                $user->setRoles(["ROLE_ADMIN"])
+                    ->setEmail("admin@admin.fr");
+            } else {
+                $user->setEmail($faker->safeEmail());
+            }
+
+            $manager->persist($user);
         }
 
         $manager->flush();
