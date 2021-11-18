502f103803edcd601f9ba9696ee8452d7dd684dc
Clydic <70671047+Clydic@users.noreply.github.com>
2021-11-18 16:32:46 +0100

    Doc : documentation sur le task controller, et le ùmailsender service

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -43,45 +43,57 @@ class TaskController extends AbstractController
     public function index(MailerService $mailer): Response
     {
 
-        $user = $this->getUser();
-        $username = explode('@', $user->getEmail())[0];
-        $task = new Task;
-        // dd($user);
+        // On intitialise la fonction
+        $user = $this->getUser(); //On récupère le User 
 
+        // On récupère le nom d'utilisateur à partir de son adresse e-mail
+        $username = explode('@', $user->getEmail())[0];
+
+        // On instantie la date d'aujourd'hui
         $now = new DateTime();
 
+        // On récupère les tâches
         $tasks = $this->repository->findAll();
+
+        // On initialise le msg 
         $msg = '';
 
+        // On boucle sur la liste des tâches
         foreach ($tasks as $task) {
-
+            // On calcule la durée qui sépare la Date d'aujourd'hui avec la date 
+            //d'échéance de la tâche.
             $diffDate = $now->diff($task->getDueAt());
 
+            // On ajoute les paramètres que l'on souhaite afficher dans le message
+            $parameters = [
+                'username' => $username,
+                'task' => $task,
+                'msg' => $msg
+            ];
+
+            /* Si la durée est inférieur ou égale 2 jours et que la date d'aujourd'hui
+            * et que la date d'aujourd'hui et antérieur à la date d'échéance
+            * on écrit un message avertissant l'utilisateur que la date arrive bientôt
+            */
             if ($diffDate->days <= 2 && ($now < $task->getDueAt())) {
 
-                $msg = ' arrive à échéance le ';
-                $parameters = [
-                    'username' => $username,
-                    'task' => $task,
-                    'msg' => $msg
-                ];
 
+                $msg = ' arrive à échéance le '; // Le bout de message d'avertissement
+
+                // On envoie l'jge-mail
                 $mailer->sendEmail("Attention ! Votre tache arrive à échéance !", $user->getEmail(), 'emails\alert.html.twig', $parameters);
-            } else if ($now > $task->getDueAt()) {
-                $msg = " a dépassé la date d'échéance le ";
-                $parameters = [
-                    'username' => $username,
-                    'task' => $task,
-                    'msg' => $msg
-                ];
 
+                // Si la durée est inférieur ou égale 2 jours et que la date d'aujourd'hui
+                // et que la date d'aujourd'hui et antérieur à la date d'échéance
+                // on écrit un message avertissant l'utilisateur que la date est passée
+            } else if ($now > $task->getDueAt()) {
+                $msg = " a dépassé la date d'échéance le "; // Le bout de message qui informe le dépassement de l'échéance
+
+                // On envoie le e-mail
                 $mailer->sendEmail("Attention ! Votre tache est arrivée à échéance !", $user->getEmail(), 'emails\alert.html.twig', $parameters);
             }
         }
 
-        // var_dump($tasks);    
-        // die;
-        //dd($tasks);
 
         return $this->render('task/index.html.twig', [
             'tasks' => $tasks,
--- src/Services/MailerService.php
+++ src/Services/MailerService.php
@@ -1,4 +1,4 @@
-<?php
+<?php // src/Services/MailerService.php
 
 namespace App\Services;
 
@@ -31,10 +31,23 @@ class MailerService
         $this->twig = $twig;
     }
 
-
-    public function sendEmail(string $subject, string $mail, string $template, array $params): void
+    /**
+     * sendEmail permet de préparer email en récupérant
+     * le sujet, l'adresse mail, le template du message
+     * et un tableau contenant les varaibles que l'on 
+     * introduira dans le corp du message
+     * @param string $subject
+     * @param string $mail
+     * @param string $template
+     * @param array $params
+     * @return void
+     */
+    public function sendEmail(string $subject, string $mail, string $template, array $params = null): void
     {
+        // On attrape l 'erreur si il y un soucis lor de l'envoi de l'email.
         try {
+            // On prépare l'email qui sera envoyé en mettant les 
+            // variables dans les différent champs du mail.
             $email = (new Email())
                 ->from($mail)
                 ->to($mail)
