6761d7b8ff017210a97d8879c36052643604e902
PaulBriand <briand.paul@outlook.fr>
2021-11-25 09:45:53 +0100

    Ajout Status + couleur

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -9,6 +9,7 @@ use Dompdf\Options;
 use App\Entity\Task;
 use PhpParser\Node\Stmt\Label;
 use Doctrine\ORM\EntityRepository;
+use App\Repository\StatusRepository;
 use Symfony\Component\Form\AbstractType;
 use Egulias\EmailValidator\Parser\DomainPart;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,6 +17,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -30,9 +32,17 @@ class TaskType extends AbstractType
      */
     private $translator;
 
-    public function __construct(TranslatorInterface $translator)
+    /**
+     * Undocumented variable
+     * 
+     * @var StatusRepository
+     */
+    private $repository;
+
+    public function __construct(TranslatorInterface $translator, StatusRepository $repository)
     {
         $this->translator = $translator;
+        $this->repository = $repository;
     }
 
 
@@ -61,6 +71,18 @@ class TaskType extends AbstractType
                 'label' => $this->translator->trans('general.category'),
                 'choice_label' => 'name'
             ])
+
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    $this->translator->trans("general.status.1") => $this->repository->findAll()[0],
+                    $this->translator->trans("general.status.2") => $this->repository->findAll()[1],
+                    $this->translator->trans("general.status.3") => $this->repository->findAll()[2]
+                ],
+                'label' => $this->translator->trans("general.status.title"),
+                'expanded' => false,
+                'multiple' => false
+            ])
+
 
             ->add('save', SubmitType::class, [
                 'label' => $this->translator->trans('general.button.success'),
